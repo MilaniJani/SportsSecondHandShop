87d0acb701c3797b39abaa5d391683d25778fbc8
MilaniJani <acmilanist91@gmail.com>
2019-10-16 12:26:18 +0900

    remove uselessFolder

--- WorkForTeamLab/src/main/java/com/wfteamlab/engineering/member/service/IMemberService.java
+++
@@ -1,8 +0,0 @@
-package com.wfteamlab.engineering.member.service;
-
-import com.wfteamlab.engineering.member.vo.ShopMember;
-
-public interface IMemberService {
-	ShopMember memberSearchOne(String id);
-	int insertMember(ShopMember shopmember);
-}
